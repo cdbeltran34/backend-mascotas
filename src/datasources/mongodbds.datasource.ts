7a22b20b17b5ce3315bab8eab7c823ea9e42525b
cristian.1701422500@ucaldas.edu.co <cristian.1701422500@ucaldas.edu.co>
2021-09-24 16:53:02 -0500

    creacion de repositorio, relaciones y migracion

--- src/datasources/mongodbds.datasource.ts
+++ src/datasources/mongodbds.datasource.ts
@@ -19,7 +19,7 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class mongodbdsDataSource extends juggler.DataSource
+export class MongodbdsDataSource extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'mongodbds';
   static readonly defaultConfig = config;
